8cab5afca40eb5d29ad9ad32ac37f215aa81d658
Miguel Julián <figarocorso@users.noreply.github.com>
2021-09-22 22:16:44 +0200

    Add defaults config file (#2)

--- src/ticket.py
+++ src/ticket.py
@@ -1,14 +1,18 @@
 class Ticket:
 
-    def __init__(self):
-        title = ""
-        description = ""
+    def __init__(self, default_config={}):
+        self.title = ""
+        self.description = ""
+        self.component = default_config["component"] if "component" in default_config else ""
+        self.epic = default_config["epic"] if "epic" in default_config else ""
 
     def __str__(self):
         return f"""
 Ticket content:
-  Title: {self.title}
-  Description: {self.description}"""
+  Title:       {self.title}
+  Description: {self.description}
+  Component:   {self.component}
+  Epic:        {self.epic}"""
 
     def ask_for_title(self):
         self.title = input("Enter title: ")
