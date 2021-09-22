5de4f24d2806f1c6e32aed4e65a9c1797fac58c6
figarocorso <figarocorso@gmail.com>
2021-09-22 22:15:33 +0200

    Add defaults config file

--- pjc.py
+++ pjc.py
@@ -1,11 +1,23 @@
 from src.ticket import Ticket
 
+import json
+
+
+CONFIG_LOCATION = "config.json"
+
 
 def main():
-    ticket = Ticket()
+    ticket = Ticket(default_config=get_default_config())
     ticket.ask_for_title()
     ticket.ask_for_description()
     print(ticket)
+
+
+def get_default_config():
+    default_config = {}
+    with open(CONFIG_LOCATION) as f:
+        default_config = json.load(f)
+    return default_config
 
 
 if __name__ == "__main__":
